c7011c32ea6c33a2fe3a5e3df157d5a408c5aa4e
Anmol <113883568+Molkaushik@users.noreply.github.com>
2022-12-23 18:49:47 +0530

    testing

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.static("public"));
 mongoose.set("strictQuery", false);
 const connectDB = async () => {
   try{
-    mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.p1eqp3g.mongodb.net/todolistDB`, {useNewUrlParser: true, useUnifiedTopology:true});
+    mongoose.connect(`mongodb+srv://admin-anmol:todolist-admin-anmol@cluster0.p1eqp3g.mongodb.net/todolistDB`, {useNewUrlParser: true, useUnifiedTopology:true});
     console.log("MongoDB Connected.");
   } catch(err){
     console.log(err);
