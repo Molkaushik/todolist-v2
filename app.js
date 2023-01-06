eb08bf8357fbf9a1db1ab69a3d03210a9db442c2
Anmol <113883568+Molkaushik@users.noreply.github.com>
2023-01-06 19:48:41 +0530

    authetication using passport

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 //jshint esversion:6
-
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const _ = require("lodash");
-const dotEnv = require("dotenv");
-//const session = require("express-session");
+const session = require("express-session");
 const passport = require("passport");
-//const passportLocalMongoose = require("passport-local-mongoose");
-
-dotEnv.config();
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
@@ -17,14 +14,14 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
 
-// app.use(session({
-//   secret: "A long string",
-//   resave: false,
-//   saveUninitialized: false
-// }));
+app.use(session({
+  secret: "A long string",
+  resave: false,
+  saveUninitialized: false
+}));
 
-// app.use(passport.initialize());
-// app.use(passport.session());
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.set("strictQuery", false);
 const connectDB = async () => {
@@ -66,128 +63,164 @@ const List = new mongoose.model("List", listSchema);
 
 const userSchema = new mongoose.Schema({
   username: String,
-  email: String,
   password: String,
   lists: [listSchema]
 });
 
-// userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(passportLocalMongoose);
 
 const User = new mongoose.model("User", userSchema);
 
-// passport.use(User.createStrategy());
+passport.use(User.createStrategy());
 
-// passport.serializeUser(User.serializeUser());
-// passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res){
   res.render("home");
 });
 
 app.get("/login", function(req, res){
-  res.render('login', {text: "Password"});
+  res.render('login', {text: "Username (letter and numbers)"});
 });
 
 app.get("/register", function(req, res){
-  res.render('register', {text: "Username"});
+  res.render('register', {text: "Username (letters and numbers)"});
 });
 
 app.get("/user", function(req, res){
   res.send("User successfully registered");
 });
 
-app.get("/:userName", function(req, res){
-  const userName = req.params.userName;
-// remember to check if the user is logged in or not
-  User.findOne({username: userName}, function(err, foundUser){
-    if(!err){
-      if(!foundUser){
-        res.redirect("/register");
-      }else{
-        if(foundUser.lists.length === 0){
-          const list = new List({
-            name: "My Day",
-            items: defaultItems
-          });
-          foundUser.lists.push(list);
-          foundUser.save();
-          res.redirect("/" + userName);
+app.get("/:username", function(req, res){
+  if(req.isAuthenticated()){
+    const username = req.params.username;
+  // remember to check if the user is logged in or not
+    User.findOne({username: username}, function(err, foundUser){
+      if(!err){
+        if(!foundUser){
+          res.redirect("/register");
         }else{
-          res.render("list", {user: userName, listTitle: foundUser.lists[0].name, newListItems: foundUser.lists[0].items});
-        }
-      }
-    }else console.log(err);
-  });
-});
-
-app.get("/:userName/:customListName", function(req, res){
-  const customListName = _.capitalize(req.params.customListName);
-  const userName = req.params.userName;
-  User.findOne({username: userName}, function(err, foundUser){
-    if(err) console.log(err);
-    else{
-      if(foundUser){
-        User.findOne({'lists.name': customListName}, function(err, foundUserList){
-          if(err) console.log(err);
-          else{
-            if(foundUserList){
-              const ind = foundUserList.lists.findIndex(list => list.name === customListName);
-              res.render("list", {user: userName, listTitle: customListName, newListItems: foundUserList.lists[ind].items});
-            }else{
-              const list = new List({
-                name: customListName,
-                items: defaultItems
-              });
-              foundUser.lists.push(list);
-              foundUser.save();
-              res.redirect("/" + userName + "/" + customListName);
-            }
+          if(foundUser.lists.length === 0){
+            const list = new List({
+              name: "My Day",
+              items: defaultItems
+            });
+            foundUser.lists.push(list);
+            foundUser.save();
+            res.redirect("/" + username);
+          }else{
+            res.render("list", {user: username, listTitle: foundUser.lists[0].name, newListItems: foundUser.lists[0].items});
           }
-        });
-      }else res.redirect("/register");
-    }
-  });
+        }
+      }else console.log(err);
+    });
+  }else{
+    res.redirect("/login");
+  }
 });
 
+app.get("/:username/:customListName", function(req, res){
+  if(req.isAuthenticated()){
+    const customListName = _.capitalize(req.params.customListName);
+    const username = req.params.username;
+    User.findOne({username: username}, function(err, foundUser){
+      if(err) console.log(err);
+      else{
+        if(foundUser){
+          User.findOne({'lists.name': customListName}, function(err, foundUserList){
+            if(err) console.log(err);
+            else{
+              if(foundUserList){
+                const ind = foundUserList.lists.findIndex(list => list.name === customListName);
+                res.render("list", {user: username, listTitle: customListName, newListItems: foundUserList.lists[ind].items});
+              }else{
+                const list = new List({
+                  name: customListName,
+                  items: defaultItems
+                });
+                foundUser.lists.push(list);
+                foundUser.save();
+                res.redirect("/" + username + "/" + customListName);
+              }
+            }
+          });
+        }else res.redirect("/register");
+      }
+    });
+  }else{
+    res.redirect("/login");
+  }
+});
+
+
+// app.post("/login", function(req, res){
+//   const userEmail = req.body.email;
+//   User.findOne({email: userEmail}, function(err, foundUser){
+//     if(foundUser){
+//       if(foundUser.password === req.body.password) res.redirect("/" + foundUser.username);
+//       else res.render("login", {text: "Incorrect Password"});
+//     }else res.redirect("/register");
+//   });
+// });
 
 app.post("/login", function(req, res){
-  const userEmail = req.body.email;
-  User.findOne({email: userEmail}, function(err, foundUser){
-    if(foundUser){
-      if(foundUser.password === req.body.password) res.redirect("/" + foundUser.username);
-      else res.render("login", {text: "Incorrect Password"});
-    }else res.redirect("/register");
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
   });
-});
-
-app.post("/register", function(req, res){
-  const userEmail = req.body.email;
-  const userName = req.body.userName;
-  User.findOne({username: userName}, function(err, foundUsername){
-    if(foundUsername){
-      res.render("register", {text: "Username already exists! Try again."});
+  req.login(user, function(err){
+    if(err){
+      console.log(err);
     }else{
-      User.findOne({email: userEmail}, function(err, foundUser){
-        if(!foundUser){
-          const user = new User({
-            username: userName,
-            email: userEmail,
-            password: req.body.password
-          });
-          user.save();
-        }
-        res.redirect("/" + userName);
+      passport.authenticate("local", { failureRedirect: '/login', failureMessage: true })(req, res, function(){
+        res.redirect("/" + req.body.username);
       });
     }
   });
 });
 
+app.post("/register", function(req, res){
+  User.register({username: req.body.username}, req.body.password, function(err, user){
+    if(err){
+      console.log(err);
+      res.render("register", {text: "Username already taken. Try Again!"});
+    }else{
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/" + req.body.username);
+      });
+    }
+  });
+});
+
+// app.post("/register", function(req, res){
+//   const userEmail = req.body.email;
+//   const userName = req.body.userName;
+//   User.findOne({username: userName}, function(err, foundUsername){
+//     if(foundUsername){
+//       res.render("register", {text: "Username already exists! Try again."});
+//     }else{
+//       User.findOne({email: userEmail}, function(err, foundUser){
+//         if(!foundUser){
+//           const user = new User({
+//             username: userName,
+//             email: userEmail,
+//             password: req.body.password
+//           });
+//           user.save();
+//         }
+//         res.redirect("/" + userName);
+//       });
+//     }
+//   });
+// });
+
 app.post("/delete", function(req, res){
   const checkedItemId = req.body.checkbox;
   const listName = req.body.listName;
-  const userName = req.body.userName;
+  const username = req.body.username;
 
-  User.findOne({username: userName}, function(err, foundUser){
+  User.findOne({username: username}, function(err, foundUser){
     if(!err){
       if(foundUser){
         if(listName === foundUser.lists[0].name){
@@ -202,18 +235,18 @@ app.post("/delete", function(req, res){
             return item._id != checkedItemId;
           });
           foundUser.save();
-          res.redirect("/" + userName + "/" + listName);
+          res.redirect("/" + username + "/" + listName);
         }
       }
     }
   });
 });
 
-app.post("/:userName", function(req, res){
-  const userName = req.params.userName;
+app.post("/:username", function(req, res){
+  const username = req.params.username;
   const newItem = req.body.newItem;
   const listName = req.body.list;
-  User.findOne({username: userName}, function(err, foundUser){
+  User.findOne({username: username}, function(err, foundUser){
     if(!err){
       if(foundUser){
         const ind = foundUser.lists.findIndex(list => list.name === listName);
@@ -223,8 +256,8 @@ app.post("/:userName", function(req, res){
         foundUser.lists[ind].items.push(item);
         foundUser.save();
       }
-      if(listName === foundUser.lists[0].name) res.redirect("/"+userName);
-      else res.redirect("/" + userName + "/" + listName);
+      if(listName === foundUser.lists[0].name) res.redirect("/"+username);
+      else res.redirect("/" + username + "/" + listName);
     }
   });
 });
